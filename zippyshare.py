3e52d97a85158c381e5e60696c4bf11be9c310ef
rrajak7499 <56112857+rrajak7499@users.noreply.github.com>
2020-07-18 12:58:08 +0530

    Create zippyshare.py

---
+++ zippyshare.py
@@ -0,0 +1,14 @@
+#*****  Step : 1  *****
+!pwd
+
+#*****  Step : 2  *****
+cd sample_data/
+
+#*****  Step : 3  *****
+!git clone https://github.com/Cartmanishere/zippyshare-scraper.git
+
+#*****  Step : 4  ***** 
+!python /content/sample_data/zippyshare-scraper/zippyshare.py
+
+#*****  Step : 5  *****
+!wget 'download link'
